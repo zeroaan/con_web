a4047e78903700b12c4e96b8889a13faa5d659ed
Jinyoung Ahn <48481448+zeroaan@users.noreply.github.com>
2020-11-21 21:52:35 +0900

    Update Navbear css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,58 +19,54 @@ const Navbar = ({ color, borderColor }) => {
 
   return (
     <>
-      <div style={{ display: "flex", justifyContent: "space-between" }}>
-        <div
-          className="nav"
-          style={{
-            borderBottom: `1px solid ${borderColor}`,
-            backgroundColor: "rgb(255,255,255,0.7)",
-          }}
-        >
-          <Link to="/">
-            <img className="logo" src={logo} alt="LOGO" />
+      <div
+        className="nav"
+        style={{
+          borderBottom: `1px solid ${borderColor}`,
+          backgroundColor: "rgb(255,255,255,0.7)",
+        }}
+      >
+        <Link to="/">
+          <img className="logo" src={logo} alt="LOGO" />
+        </Link>
+        <div className="navbar">
+          <Link to="/concept" style={{ color: `${color}` }}>
+            Concept
           </Link>
-          <ul className="navbar">
-            <Link to="/concept" style={{ color: `${color}` }}>
-              <li>Concept</li>
-            </Link>
-            <Link to="/progress" style={{ color: `${color}` }}>
-              <li>Progress</li>
-            </Link>
-            <Link to="/stack" style={{ color: `${color}` }}>
-              <li>Stack</li>
-            </Link>
-            <Link to="/operation" style={{ color: `${color}` }}>
-              <li>Operation</li>
-            </Link>
-            <Link to="/member" style={{ color: `${color}` }}>
-              <li>Member</li>
-            </Link>
-          </ul>
-          {loginState ? (
-            <>
-              <div className="loginSuccess">
-                <img
-                  className="loginSuccessImg"
-                  src={loginSuccess}
-                  alt="loginSuccess"
-                />
-              </div>
-              <Link
-                className="logoutBt"
-                to="/"
-                style={{ color: `${color}` }}
-                onClick={onClickLogoutBt}
-              >
-                logout
-              </Link>
-            </>
-          ) : (
-            <Link className="loginBt" to="/login" style={{ color: `${color}` }}>
-              login
-            </Link>
-          )}
+          <Link to="/progress" style={{ color: `${color}` }}>
+            Progress
+          </Link>
+          <Link to="/stack" style={{ color: `${color}` }}>
+            Stack
+          </Link>
+          <Link to="/operation" style={{ color: `${color}` }}>
+            Operation
+          </Link>
+          <Link to="/member" style={{ color: `${color}` }}>
+            Member
+          </Link>
         </div>
+        {loginState ? (
+          <>
+            <img
+              className="loginSuccessImg"
+              src={loginSuccess}
+              alt="loginSuccess"
+            />
+            <Link
+              className="logoutBt"
+              to="/"
+              style={{ color: `${color}` }}
+              onClick={onClickLogoutBt}
+            >
+              logout
+            </Link>
+          </>
+        ) : (
+          <Link className="loginBt" to="/login" style={{ color: `${color}` }}>
+            login
+          </Link>
+        )}
       </div>
     </>
   );
