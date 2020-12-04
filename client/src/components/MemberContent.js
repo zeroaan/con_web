780100adb95731ff37ce1eb6e15b975bf08bc421
Jinyoung Ahn <48481448+zeroaan@users.noreply.github.com>
2020-12-04 10:47:11 +0900

    Update member

---
+++ client/src/components/MemberContent.js
@@ -0,0 +1,64 @@
+import React from "react";
+import styled from "styled-components";
+
+import GitHubIcon from "@material-ui/icons/GitHub";
+import { makeStyles } from "@material-ui/core/styles";
+
+const DivMember = styled.div`
+  margin: 0px 70px;
+`;
+const ImgStyled = styled.img`
+  width: 180px;
+  height: 180px;
+`;
+const PName = styled.p`
+  width: 250px;
+  margin: 20px 0;
+  font-size: 23px;
+  font-family: "Nunito", sans-serif;
+`;
+const DivDesc = styled.div`
+  width: 250px;
+  border-top: 3px solid rgba(0, 0, 0, 0.1);
+  border-bottom: 3px solid rgba(0, 0, 0, 0.1);
+  margin: 20px 0;
+  padding: 20px 0;
+`;
+const AGit = styled.a`
+  text-decoration: none;
+  color: rgba(0, 0, 0, 0.8);
+`;
+
+const useStyles = makeStyles({
+  gitIcon: {
+    margin: "20px 0 0 0",
+    fontSize: "40px",
+    color: "rgba(0, 0, 0, 0.8)",
+    transition: "all 0.3s ease",
+    "&:hover": {
+      color: "black",
+      transform: "scale(1.1)",
+    },
+  },
+});
+
+const MemberContent = ({ img, name, desc, git }) => {
+  const classes = useStyles();
+
+  return (
+    <>
+      <DivMember>
+        <ImgStyled src={img} alt="FRODO" />
+        <PName>{name}</PName>
+        <DivDesc>
+          <p>{desc.text}</p>
+          <AGit href={git} target="_blank">
+            <GitHubIcon className={classes.gitIcon} />
+          </AGit>
+        </DivDesc>
+      </DivMember>
+    </>
+  );
+};
+
+export default MemberContent;
--- client/src/pages/Members.js
+++ client/src/pages/Members.js
@@ -1,17 +1,19 @@
 import React from "react";
-import GitHubIcon from "@material-ui/icons/GitHub";
-import MemoryIcon from "@material-ui/icons/Memory";
-import LanguageIcon from "@material-ui/icons/Language";
 import styled from "styled-components";
 
-import CON from "assets/member/con1.png";
-import RYAN from "assets/member/ryan.png";
-import MUZI from "assets/member/muzi.png";
+import MemoryIcon from "@material-ui/icons/Memory";
+import LanguageIcon from "@material-ui/icons/Language";
+import { makeStyles } from "@material-ui/core/styles";
+
 import FRODO from "assets/member/frodo.png";
+import CON from "assets/member/con1.png";
+import MUZI from "assets/member/muzi.png";
+import RYAN from "assets/member/ryan.png";
 import APEACH from "assets/member/apeach.png";
 
 import Navbar from "components/Navbar";
 import AboutTitle from "components/AboutTitle";
+import MemberContent from "components/MemberContent";
 import Footer from "components/Footer";
 
 const DivCon = styled.div`
@@ -19,206 +21,111 @@ const DivCon = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  margin-top: 30px;
+  margin: 110px 0 80px 0;
+`;
+const DivTeamTitle = styled.div`
+  display: flex;
+  margin: 0 38px 0 0;
+`;
+const H1TeamTitle = styled.h1`
+  font-family: "Nunito", sans-serif;
+  color: rgba(0, 0, 0, 0.8);
+  border-bottom: 3px solid rgba(0, 0, 0, 0.3);
+  margin: 0 15px;
 `;
 const DivTeam = styled.div`
   display: flex;
   text-align: center;
-  margin: 50px 0;
-`;
-const DivMember = styled.div`
-  margin: 0px 70px;
-`;
-const ImgStyled = styled.img`
-  width: 180px;
-  height: 180px;
+  margin: 50px 0 0 0;
 `;
 
+const useStyles = makeStyles({
+  teamIcon: {
+    color: "rgba(0, 0, 0, 0.8)",
+    fontSize: "43px",
+  },
+});
+
 const Members = () => {
+  const classes = useStyles();
+
+  const embededTeam = [
+    {
+      img: `${FRODO}`,
+      name: "이지형",
+      desc: {
+        text:
+          "Introduce your business team in the perfect manner with our make a team introduction presentation about us PowerPoint slide. The presentation design shares the concept.",
+      },
+      git: "https://github.com/",
+    },
+    {
+      img: `${CON}`,
+      name: "변상준",
+      desc: {
+        text:
+          "Introduce your business team in the perfect manner with our make a team introduction presentation about us PowerPoint slide. The presentation design shares the concept.",
+      },
+      git: "https://github.com/",
+    },
+  ];
+  const webTeam = [
+    {
+      img: `${MUZI}`,
+      name: "안진영",
+      desc: {
+        text:
+          "Introduce your business team in the perfect manner with our make a team introduction presentation about us PowerPoint slide. The presentation design shares the concept.",
+      },
+      git: "https://github.com/",
+    },
+    {
+      img: `${RYAN}`,
+      name: "이현재",
+      desc: {
+        text:
+          "Introduce your business team in the perfect manner with our make a team introduction presentation about us PowerPoint slide. The presentation design shares the concept.",
+      },
+      git: "https://github.com/",
+    },
+    {
+      img: `${APEACH}`,
+      name: "최유솜",
+      desc: {
+        text:
+          "Introduce your business team in the perfect manner with our make a team introduction presentation about us PowerPoint slide. The presentation design shares the concept.",
+      },
+      git: "https://github.com/",
+    },
+  ];
+
   return (
     <>
       <Navbar />
       <AboutTitle
         title="Member"
-        subTitle="We're looking for great people to join our growing team."
+        subTitle="We're creating solutions for the future of automated logistics warehouse."
       />
       <DivCon>
-        <div style={{ display: "flex", position: "relative", left: "-25px" }}>
-          <MemoryIcon
-            style={{
-              color: "rgba(0,0,0,0.8)",
-              margin: "80px 15px 0 0",
-              fontSize: "43px",
-            }}
-          />
-          <h1
-            style={{
-              marginTop: "80px",
-              color: "rgba(0,0,0,0.8)",
-              borderBottom: "3px solid rgba(0,0,0,0.3)",
-              fontFamily: `"Nunito", sans-serif`,
-            }}
-          >
-            Embedded
-          </h1>
-        </div>
+        <DivTeamTitle>
+          <MemoryIcon className={classes.teamIcon} />
+          <H1TeamTitle>Embedded</H1TeamTitle>
+        </DivTeamTitle>
         <DivTeam>
-          <DivMember>
-            <ImgStyled src={FRODO} alt="FRODO" />
-            <p
-              style={{
-                width: "250px",
-                margin: "20px 0",
-                fontSize: "23px",
-              }}
-            >
-              이지형
-            </p>
-            <div
-              style={{
-                width: "250px",
-                borderTop: "3px solid rgba(0,0,0,0.1)",
-                borderBottom: "3px solid rgba(0,0,0,0.1)",
-                margin: "20px 0",
-                padding: "20px 0",
-              }}
-            >
-              <p>
-                Introduce your business team in the perfect manner with our make a team introduction
-                presentation about us PowerPoint slide. The presentation design shares the concept.
-              </p>
-              <GitHubIcon style={{ margin: "20px 0 0 0", fontSize: "35px" }} />
-            </div>
-          </DivMember>
-          <DivMember>
-            <ImgStyled src={CON} alt="CON" />
-            <p
-              style={{
-                width: "250px",
-                margin: "20px 0",
-                fontSize: "23px",
-              }}
-            >
-              변상준
-            </p>
-            <div
-              style={{
-                width: "250px",
-                borderTop: "3px solid rgba(0,0,0,0.1)",
-                borderBottom: "3px solid rgba(0,0,0,0.1)",
-                margin: "20px 0",
-                padding: "20px 0",
-              }}
-            >
-              <p>
-                Introduce your business team in the perfect manner with our make a team introduction
-                presentation about us PowerPoint slide. The presentation design shares the concept.
-              </p>
-              <GitHubIcon style={{ margin: "20px 0 0 0", fontSize: "35px" }} />
-            </div>
-          </DivMember>
+          {embededTeam.map((v) => (
+            <MemberContent key={v.name} img={v.img} name={v.name} desc={v.desc} git={v.git} />
+          ))}
         </DivTeam>
-        <div style={{ display: "flex", position: "relative", left: "-25px" }}>
-          <LanguageIcon
-            style={{
-              color: "rgba(0,0,0,0.8)",
-              margin: "80px 15px 0 0",
-              fontSize: "43px",
-            }}
-          />
-          <h1
-            style={{
-              marginTop: "80px",
-              color: "rgba(0,0,0,0.8)",
-              borderBottom: "3px solid rgba(0,0,0,0.3)",
-              fontFamily: `"Nunito", sans-serif`,
-            }}
-          >
-            Web
-          </h1>
-        </div>
+      </DivCon>
+      <DivCon>
+        <DivTeamTitle>
+          <LanguageIcon className={classes.teamIcon} />
+          <H1TeamTitle>Web</H1TeamTitle>
+        </DivTeamTitle>
         <DivTeam>
-          <DivMember>
-            <ImgStyled src={MUZI} alt="MUZI" />
-            <p
-              style={{
-                width: "250px",
-                margin: "20px 0",
-                fontSize: "23px",
-              }}
-            >
-              안진영
-            </p>
-            <div
-              style={{
-                width: "250px",
-                borderTop: "3px solid rgba(0,0,0,0.1)",
-                borderBottom: "3px solid rgba(0,0,0,0.1)",
-                margin: "20px 0",
-                padding: "20px 0",
-              }}
-            >
-              <p>
-                Introduce your business team in the perfect manner with our make a team introduction
-                presentation about us PowerPoint slide. The presentation design shares the concept.
-              </p>
-              <GitHubIcon style={{ margin: "20px 0 0 0", fontSize: "35px" }} />
-            </div>
-          </DivMember>
-          <DivMember>
-            <ImgStyled src={RYAN} alt="RYAN" />
-            <p
-              style={{
-                width: "250px",
-                margin: "20px 0",
-                fontSize: "23px",
-              }}
-            >
-              이현재
-            </p>
-            <div
-              style={{
-                width: "250px",
-                borderTop: "3px solid rgba(0,0,0,0.1)",
-                borderBottom: "3px solid rgba(0,0,0,0.1)",
-                margin: "20px 0",
-                padding: "20px 0",
-              }}
-            >
-              <p>
-                Introduce your business team in the perfect manner with our make a team introduction
-                presentation about us PowerPoint slide. The presentation design shares the concept.
-              </p>
-              <GitHubIcon style={{ margin: "20px 0 0 0", fontSize: "35px" }} />
-            </div>
-          </DivMember>
-          <DivMember>
-            <ImgStyled src={APEACH} alt="APEACH" />
-            <p
-              style={{
-                width: "250px",
-                margin: "20px 0",
-                fontSize: "23px",
-              }}
-            >
-              최유솜
-            </p>
-            <div
-              style={{
-                width: "250px",
-                borderTop: "3px solid rgba(0,0,0,0.1)",
-                borderBottom: "3px solid rgba(0,0,0,0.1)",
-                margin: "20px 0",
-                padding: "20px 0",
-              }}
-            >
-              <p>
-                Introduce your business team in the perfect manner with our make a team introduction
-                presentation about us PowerPoint slide. The presentation design shares the concept.
-              </p>
-              <GitHubIcon style={{ margin: "20px 0 0 0", fontSize: "35px" }} />
-            </div>
-          </DivMember>
+          {webTeam.map((v) => (
+            <MemberContent key={v.name} img={v.img} name={v.name} desc={v.desc} git={v.git} />
+          ))}
         </DivTeam>
       </DivCon>
       <Footer />
