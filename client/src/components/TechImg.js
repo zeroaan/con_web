d74b00060bb12d6d72304513ddb6426e8edaafc7
Jinyoung Ahn <48481448+zeroaan@users.noreply.github.com>
2020-12-04 11:26:44 +0900

    Remove tech hover

--- client/src/components/TechImg.js
+++ client/src/components/TechImg.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 
 const DivImg = styled.div`
@@ -6,42 +6,16 @@ const DivImg = styled.div`
   width: 200px;
   height: 200px;
   margin: 0 35px;
-  cursor: pointer;
 `;
 const ImgTech = styled.img`
   width: 200px;
 `;
-const DivImgHover = styled.div`
-  position: absolute;
-  top: 0px;
-  left: 0px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: rgba(0, 0, 0, 0.6);
-  width: 200px;
-  height: 200px;
-  color: white;
-  font-size: 30px;
-  font-family: "Nunito", sans-serif;
-  border-radius: 5px;
-`;
 
 const TechImg = ({ name, src }) => {
-  const [imgDesc, setImgDesc] = useState(false);
-
-  const onMouseEnter = () => {
-    setImgDesc(true);
-  };
-  const onMouseLeave = () => {
-    setImgDesc(false);
-  };
-
   return (
     <>
-      <DivImg onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
+      <DivImg>
         <ImgTech src={src} alt={name} />
-        {imgDesc ? <DivImgHover>{name}</DivImgHover> : null}
       </DivImg>
     </>
   );
