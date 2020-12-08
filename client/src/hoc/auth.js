228a543032cba6d8ace4bee30648e43c3b544e72
Jinyoung Ahn <48481448+zeroaan@users.noreply.github.com>
2020-12-08 11:16:43 +0900

    auth

--- client/src/hoc/auth.js
+++ client/src/hoc/auth.js
@@ -4,25 +4,15 @@ import { useDispatch } from "react-redux";
 import { authRequest } from "store/actions/user";
 
 const Auth = (SpecificComponent, option, adminRoute = null) => {
-  // adminRouth = null : 기본적으로 null값이 들어감. true : admin유저만 들어갈 수 있음.
-  // <Route exact path="/" component={Auth(LandingPage, null, null)} />;
-
-  // SpecificComponent : Wrapped Component
-  // option
-  // null : 아무나 출입이 가능한 페이지
-  // true : 로그인 한 유저만 출입이 가능한 페이지
-  // false : 로그인 한 유저는 출입이 불가능 한 페이지
-  // adminRouth : Admin user 출입기능
-  function AuthenticationCheck() {
+  const AuthenticationCheck = () => {
     const history = useHistory();
     const dispatch = useDispatch();
     useEffect(() => {
       dispatch(authRequest(option, adminRoute, history));
       // eslint-disable-next-line
     }, []);
-
     return <SpecificComponent />;
-  }
+  };
   return AuthenticationCheck;
 };
 
