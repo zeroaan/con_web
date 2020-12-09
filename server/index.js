021addaff9e51a7b20224b1bed9c86755913f339
Jinyoung Ahn <48481448+zeroaan@users.noreply.github.com>
2020-12-09 09:54:34 +0900

    heroku setting

--- server/index.js
+++ server/index.js
@@ -115,7 +115,7 @@ app.get("/api/users/logout", auth, (req, res) => {
 });
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(__dirname + "/client/build"));
+  app.use(express.static("client/build"));
 
   // app.get("*", (req, res) => {
   //   res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
