aa5aed816b6bcb6673d4704050e580743533ef4f
Jinyoung Ahn <48481448+zeroaan@users.noreply.github.com>
2020-12-09 10:54:26 +0900

    heroku setting

--- server/index.js
+++ server/index.js
@@ -117,8 +117,8 @@ app.get("/api/users/logout", auth, (req, res) => {
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
-  app.post("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
