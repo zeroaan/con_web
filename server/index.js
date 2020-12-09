0a84ac3f4c2cacc16f25d37d7996c7efe664c5f9
Jinyoung Ahn <48481448+zeroaan@users.noreply.github.com>
2020-12-09 10:39:31 +0900

    heroku setting

--- server/index.js
+++ server/index.js
@@ -117,9 +117,9 @@ app.get("/api/users/logout", auth, (req, res) => {
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
-  // app.get("*", (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
-  // });
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+  });
 }
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
