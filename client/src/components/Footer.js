23fad6df8ad9ed1bb1eac9d3ec32ec598d1e8bc9
Jinyoung Ahn <48481448+zeroaan@users.noreply.github.com>
2020-12-10 15:25:52 +0900

    footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,16 +1,30 @@
 import React from "react";
+import styled from "styled-components";
+
+const DivFoot = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: "Nunito" sans-serif;
+  height: 70px;
+  border-top: 1px solid silver;
+  width: 1300px;
+  margin: 120px auto 0px auto;
+`;
+const PFoot = styled.p`
+  font-size: 13px;
+  margin: 3px 0;
+  color: gray;
+  letter-spacing: 1px;
+  word-spacing: 2px;
+`;
 
 const Footer = () => {
   return (
     <>
-      <div
-        style={{
-          maxWidth: "100vw",
-          height: "150px",
-          backgroundColor: "rgba(0,0,0,0.1)",
-          marginTop: "150px",
-        }}
-      ></div>
+      <DivFoot>
+        <PFoot>Copyright &copy;{new Date().getFullYear()} Con, Inc.</PFoot>
+      </DivFoot>
     </>
   );
 };
--- client/src/pages/History.js
+++ client/src/pages/History.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import Navbar from "components/Navbar";
 import AboutTitle from "components/AboutTitle";
+import Footer from "components/Footer";
 
 const History = () => {
   return (
@@ -12,6 +13,7 @@ const History = () => {
         subTitle="We're creating solutions for the future of automated logistics warehouse."
       />
       <div>history</div>
+      <Footer />
     </>
   );
 };
